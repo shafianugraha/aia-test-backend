39356d48781d9769af406af7511297bf4fec1c96
Shafianugraha Hariananda <shafianugrahadwi@gmail.com>
2023-01-13 05:38:47 +0700

    fixing vercel config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 
 const app = express();
-const feedRoute = require('./routes/feed');
+const feedRoute = require('./app/routes/feed');
 
 app.use(cors())
 app.use('/api/v1', feedRoute);
